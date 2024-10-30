2ba73064392c98a9f034b0719b5a3fada2b4edf4
khangltt2003 <khangltt2003@gmail.com>
2024-10-30 01:46:23 -0500

    first commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+import { GhostIcon } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return <div className="text-3xl text-indigo-500 font-bold ">This is a projected route.</div>;
+}
